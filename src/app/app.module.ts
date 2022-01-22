e679b6df31cd3bd1e9826d3f612d57e49b6af5e5
John Schaub <schaubjs@gmail.com>
2022-01-22 09:57:16 -0700

    added modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutFullModule } from './layouts/layout-full/layout-full.module';
+import { LayoutSideModule } from './layouts/layout-side/layout-side.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    LayoutSideModule,
+    LayoutFullModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
