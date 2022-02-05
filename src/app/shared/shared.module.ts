45cacf6a53077e7ff25b5676e77aecfa665b1f45
John Schaub <schaubjs@gmail.com>
2022-02-04 21:40:56 -0700

    Added layout to footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [FooterComponent, HeaderComponent, SidebarComponent, NavigationComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, AppRoutingModule, FlexLayoutModule, AppRoutingModule],
   exports: [FooterComponent, HeaderComponent, SidebarComponent, NavigationComponent],
 })
 export class SharedModule {}
